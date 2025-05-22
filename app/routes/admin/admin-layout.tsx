e48ff8cd45452b2a132d32654d3333f5231d66e6
eyob13-coder <eyobgeremew618@gmail.com>
2025-05-22 16:10:57 +0300

    Routing

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,17 @@
+
+import { Outlet } from 'react-router'
+
+const AdminLayout = () => {
+  return (
+    <div className='admin-layout'>
+        AdminLayout
+        <aside className='w-full max-w-[270px] hidden lg:block'>Sidebat
+            </aside>
+            <aside className="children">
+                <Outlet/>
+            </aside>
+            </div>
+  )
+}
+
+export default AdminLayout
